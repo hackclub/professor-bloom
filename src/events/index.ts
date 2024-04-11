5afad1ea21e27a726b37fc2081214adbfb7c2a9b
Jasper Mayone <me@jaspermayone.com>
2024-04-11 07:35:24 -0400

    add dev env

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,2 @@
 // export { default as ChannelCreate } from "./channelCreate";
-// export { default as AppMention } from "./appMention";
-
-export { default as message } from "./message";
+export { default as AppMention } from "./appMention";
