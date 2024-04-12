04cba17a3048a1467f11429713c35993ce8a809f
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-04-12 19:17:57 +0530

    Bug fix: fixes runtime error due to directory not found

--- src/commands/add-template.ts
+++ src/commands/add-template.ts
@@ -7,16 +7,23 @@ const addTemplate = async (app: App): Promise<void> => {
         await ack();
 
         const { user_id, user_name, text } = command;
+        const directoryPath = path.resolve(__dirname, `../../templates`);
         const filePath = path.resolve(__dirname, `../../templates/${user_name}`);
 
         // Perform access control here
         // For you Jasper: if (!isUserAllowed(user_id)) { await respond(`Unauthorized!`); return; }
 
         try {
+
+            await fs.access(directoryPath).catch(async () => {
+                await fs.mkdir(directoryPath);
+            });
+
             // Check if file already exists
             const fileExists = await fs.access(filePath).then(() => true).catch(() => false);
 
             if (!fileExists) {
+
                 await fs.writeFile(filePath, text);
                 await respond(`File created successfully!`);
             } else {
