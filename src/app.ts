d256e3ce732802f34d63a0ea890c41b956509d9c
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-04-14 21:05:06 +0530

    Implements oAuth

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import { App } from "@slack/bolt";
 import { receiver } from "./express-receiver";
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN!,
-  appToken: process.env.SLACK_APP_TOKEN!,
   signingSecret: process.env.SLACK_SIGNING_SECRET!,
   receiver,
 });
