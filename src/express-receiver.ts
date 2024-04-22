bd5924be2c1166cadfff307b236c6f2b80bea2b2
Kartikey Chauhan <skartikey314@gmail.com>
2024-04-23 00:12:53 +0530

    Implements oAuth (#17)

--- src/express-receiver.ts
+++ src/express-receiver.ts
@@ -1,7 +1,52 @@
 import { ExpressReceiver } from "@slack/bolt";
+import { ConsoleLogger, LogLevel } from '@slack/logger';
+import { PrismaClient } from '@prisma/client';
+import { PrismaInstallationStore } from '@seratch_/bolt-prisma';
 
+const logger = new ConsoleLogger();
+logger.setLevel(LogLevel.DEBUG);
+
+const prismaClient = new PrismaClient({ log: [{ emit: 'stdout', level: 'query' }] });
+const installationStore = new PrismaInstallationStore({
+  prismaTable: prismaClient.slackToken,
+  clientId: process.env.SLACK_CLIENT_ID,
+  logger,
+});
 const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET!,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'my-secret',
+  scopes: [
+    "app_mentions:read",
+    "calls:read",
+    "canvases:read",
+    "channels:history",
+    "channels:join",
+    "channels:read",
+    "chat:write",
+    "chat:write.public",
+    "commands",
+    "emoji:read",
+    "groups:history",
+    "groups:write",
+    "im:history",
+    "im:read",
+    "im:write",
+    "metadata.message:read",
+    "reactions:read",
+    "reactions:write",
+    "users.profile:read",
+    "users:read",
+    "users:read.email",
+    "users:write"
+  ],
+  installerOptions: {
+    directInstall: true,
+    userScopes: ['chat:write'],
+  },
+  installationStore,
+  logger,
 });
 
 export { receiver };
