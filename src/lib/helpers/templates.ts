513f0407335235752b1ea06df780a0016d72cf4f
Ray <me@rayhanadev.com>
2024-05-09 02:44:58 +0000

    feat: add user helpers, move to helpers folder

--- src/lib/helpers/templates.ts
+++ src/lib/helpers/templates.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
-import { db } from "../db";
-import { templates } from "../db/schema";
+import { db } from "../../db";
+import { templates } from "../../db/schema";
 
 export type Template = {
 	id: number;
