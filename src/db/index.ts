b0ed856aa6abe017b52c3589d4c7bae0c29824e0
Jasper Mayone <me@jaspermayone.com>
2024-06-01 18:12:42 -0400

    remove drizzle

--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
-
-import * as schema from './schema';
-
-export const connection = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(connection, { schema });
