69df9990744b5a005e944db4248ae18b8d353e59
Vic <vicxd@pm.me>
2025-09-24 16:53:58 +0200

    Add charon webhook support (#46)
    * Add charon webhook support
    
    * fix typos

--- src/endpoints/webhooks/charon.ts
+++ src/endpoints/webhooks/charon.ts
@@ -1,31 +1,46 @@
 interface WebhookData {
   user_id: string;
+  program_id: string;
+  program_name: string;
+  action: "promotion";
 }
 
-function isValidWebhookData(data: any) {
+function isValidWebhookData(data: any): data is WebhookData {
   if (!data.user_id) return false;
-
+  if (!data.program_name) return false;
+  if (!data.program_id) return false;
+  if (!data.action || (data.action !== "promotion" )) return false;
   return true;
 }
 
 import { Request, Response } from "express";
-import { app, slackClient } from "../../index";
+import { slackClient } from "../../index";
 import { handleNewWelcomeable } from "../../handlers/newWelcomeable";
-import { WebClient } from "@slack/web-api";
 const valid_upgrade_webhook_tokens =
   process.env.UPGRADE_WEBHOOK_TOKENS?.split(" ");
-export async function upgradedWebhook(req: Request, res: Response) {
+
+
+  export async function charonWebhook(req: Request, res: Response) {
   if (!valid_upgrade_webhook_tokens?.includes(req?.params["token"]))
     return res.status(401).json({ message: "unauthorized" });
 
   const body: WebhookData = req.body;
+  console.log(body)
   if (!isValidWebhookData(body))
     return res.status(400).json({ message: "malformed request" });
 
-  try {
-    await handleNewWelcomeable(body.user_id, slackClient, "legacy_upgrade");
+
+  try {  
+    await handleNewWelcomeable(
+      body.user_id,
+      slackClient,
+      body.action,
+      body.program_id,
+      body.program_name
+    );
   } catch (error) {
     console.error("Error handling new upgrade:", error);
+
     return res.status(500).json({ message: "internal server error" });
   }
 
