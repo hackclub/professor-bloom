5a6f936f5ce8b5b5d87594e0e53635c22449a8bc
Vic <vicxd@pm.me>
2026-01-26 23:19:16 +0100

    Add logging for user dispatch to instrumentation

--- src/handlers/instrumentation.ts
+++ src/handlers/instrumentation.ts
@@ -9,7 +9,8 @@ export async function instrumentationRecordUser(u:UserTelData) {
     const ORCHID_API_KEY = process.env.ORCHID_API_KEY;
     if (!ORCHID_API_KEY) return;
 
-    await fetch("https://orchid.professorbloom.hackclub.com/instrumentation/user", {
+    console.log("Trying to dispatch user to instrumentation")
+    const res = await fetch("https://orchid.professorbloom.hackclub.com/instrumentation/user", {
         headers: {
             "Content-Type":"application/json",
             "X-API-Key":ORCHID_API_KEY
@@ -17,4 +18,9 @@ export async function instrumentationRecordUser(u:UserTelData) {
         method: "POST",
         body: JSON.stringify(u)
     });
+
+    if (!res.ok) {
+        console.log("Failed to dispatch welcomeable to instrumentation", res.statusText)
+    }
+
 }
